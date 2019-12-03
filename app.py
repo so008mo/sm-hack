6fc7532dad840b7bc44d2c1a40c532f009f61f0e
so008mo <58431916+so008mo@users.noreply.github.com>
2019-12-03 05:50:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,9 @@ from flask import Flask, render_template
 application =  Flask(__name__)
 
 @application.route('/')
-def home():
-    try:
-        return render_template("index.html")
-    except Exception as e:
-        return str(e)
+def hello():
+    message = "Hello, World"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     application.run(debug = True)
